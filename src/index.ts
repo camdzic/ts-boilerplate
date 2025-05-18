4a94fc871b6c1119626eb77fa38c45beacbf77f9
Aldin Čamdžić <acamdzic0@proton.me>
2025-05-18 14:15:08 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+interface Options {
+  name: string;
+  age: number;
+}
+
+function createPerson(options: Options) {
+  return {
+    name: options.name,
+    age: options.age
+  };
+}
+
+const person = createPerson({ name: 'John', age: 30 });
+console.log(person);
